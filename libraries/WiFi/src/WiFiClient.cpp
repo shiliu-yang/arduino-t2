af193fa9f7b2b0f33f5db8e75cd3708626b5434d
yangjie <shiliu.yang@tuya.com>
2023-12-29 15:03:55 +0800

    add WiFiClient

---
+++ libraries/WiFi/src/WiFiClient.cpp
@@ -0,0 +1,222 @@
+#include "WiFiClient.h"
+
+extern "C" {
+#include "tal_memory.h"
+#include "lwip/sockets.h"
+#include "lwip/netdb.h"
+#include "errno.h"
+}//extern "C"
+
+extern "C" void bk_printf(const char *fmt, ...);
+
+#define DEBUG_PRINTF bk_printf
+
+#undef connect
+#undef write
+#undef read
+
+WiFiClient::WiFiClient()
+: _timeoutMs(3000)
+, _sockfd(-1)
+, _rxBuff(nullptr)
+{
+
+}
+
+WiFiClient::~WiFiClient()
+{
+  stop();
+}
+
+int WiFiClient::connect(IPAddress ip, uint16_t port)
+{
+  return connect(ip, port, _timeoutMs);
+}
+
+int WiFiClient::connect(IPAddress ip, uint16_t port, uint32_t timeoutMs)
+{
+  _timeoutMs = timeoutMs;
+  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sockfd < 0) {
+    DEBUG_PRINTF("socket: %d", errno);
+    return 0;
+  }
+  fcntl( sockfd, F_SETFL, fcntl( sockfd, F_GETFL, 0 ) | O_NONBLOCK );
+
+  struct sockaddr_in serveraddr;
+  memset((char *) &serveraddr, 0, sizeof(serveraddr));
+  serveraddr.sin_family = AF_INET;
+  serveraddr.sin_addr.s_addr = (in_addr_t)ip;
+  serveraddr.sin_port = htons(port);
+  fd_set fdset;
+  struct timeval tv;
+  FD_ZERO(&fdset);
+  FD_SET(sockfd, &fdset);
+  tv.tv_sec = _timeoutMs / 1000;
+  tv.tv_usec = (_timeoutMs  % 1000) * 1000;
+
+  int res = lwip_connect(sockfd, (struct sockaddr*)&serveraddr, sizeof(serveraddr));
+  if (res < 0 && errno != EINPROGRESS) {
+    DEBUG_PRINTF("connect on fd %d, errno: %d, \"%s\"", sockfd, errno, strerror(errno));
+    close(sockfd);
+    return 0;
+  }
+
+  res = select(sockfd + 1, nullptr, &fdset, nullptr, _timeoutMs<0 ? nullptr : &tv);
+  if (res < 0) {
+    DEBUG_PRINTF("select on fd %d, errno: %d, \"%s\"", sockfd, errno, strerror(errno));
+    close(sockfd);
+    return 0;
+  } else if (res == 0) {
+    DEBUG_PRINTF("select returned due to timeout %d ms for fd %d", _timeoutMs, sockfd);
+    close(sockfd);
+    return 0;
+  } else {
+    int sockerr;
+    socklen_t len = (socklen_t)sizeof(int);
+    res = getsockopt(sockfd, SOL_SOCKET, SO_ERROR, &sockerr, &len);
+
+    if (res < 0) {
+      DEBUG_PRINTF("getsockopt on fd %d, errno: %d, \"%s\"", sockfd, errno, strerror(errno));
+      close(sockfd);
+      return 0;
+    }
+
+    if (sockerr != 0) {
+      DEBUG_PRINTF("socket error on fd %d, errno: %d, \"%s\"", sockfd, sockerr, strerror(sockerr));
+      close(sockfd);
+      return 0;
+    }
+  }
+
+#define ROE_WIFICLIENT(x,msg) { if (((x)<0)) { DEBUG_PRINTF("Setsockopt '" msg "'' on fd %d failed. errno: %d, \"%s\"", sockfd, errno, strerror(errno)); return 0; }}
+  ROE_WIFICLIENT(setsockopt(sockfd, SOL_SOCKET, SO_SNDTIMEO, &tv, sizeof(tv)),"SO_SNDTIMEO");
+  ROE_WIFICLIENT(setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv)),"SO_RCVTIMEO");
+
+  fcntl( sockfd, F_SETFL, fcntl( sockfd, F_GETFL, 0 ) & (~O_NONBLOCK) );
+  _sockfd = sockfd;
+  if (_rxBuff) {
+    _rxBuff->flush();
+  }
+
+  _connected = true;
+  return 1;
+}
+
+int WiFiClient::connect(const char *host, uint16_t port)
+{
+  return 1;
+}
+size_t WiFiClient::write(uint8_t)
+{
+  return 0;
+}
+size_t WiFiClient::write(const uint8_t *buf, size_t size)
+{
+  return 0;
+}
+int WiFiClient::available()
+{
+  read(nullptr, 0);
+  return (int)_rxBuff->available();
+}
+
+int WiFiClient::read()
+{
+  uint8_t readVal;
+  read(&readVal, 1);
+
+  return (int)readVal;
+}
+
+int WiFiClient::read(uint8_t *buf, size_t size)
+{
+  int count = 0;
+  int res = lwip_ioctl(_sockfd, FIONREAD, &count);
+  if (res<0 || count==0) {
+    return 0;
+  }
+
+  if (nullptr == _rxBuff) {
+    _rxBuff = new cbuf(TCP_RX_PACKET_MAX_SIZE);
+    if (!_rxBuff) {
+      return -1;
+    }
+  }
+
+  uint8_t *tmpBuff = (uint8_t *)tal_malloc(count);
+  if (!tmpBuff) {
+    return -1;
+  }
+
+  res = recv(_sockfd, tmpBuff, count, MSG_DONTWAIT);
+  if(res < 0) {
+    return 0;
+  }
+  _rxBuff->write((const char*)tmpBuff, (size_t)count);
+  tal_free(tmpBuff);
+
+  if (!buf && size > 0) {
+    _rxBuff->read((char*)buf, size);
+  }
+
+  return res;
+}
+
+int WiFiClient::peek()
+{
+  read(nullptr, 0);
+  return _rxBuff->peek();
+}
+
+void WiFiClient::flush()
+{
+  while (available()) {
+    _rxBuff->flush();
+  }
+}
+
+void WiFiClient::stop()
+{
+  close(_sockfd);
+  _sockfd = -1;
+  if (_rxBuff) {
+    cbuf *b = _rxBuff;
+    _rxBuff = nullptr;
+    delete b;
+  }
+}
+
+uint8_t WiFiClient::connected()
+{
+  if (_connected) {
+    uint8_t dummy;
+    int res = recv(_sockfd, &dummy, 0, MSG_DONTWAIT);
+    // avoid unused var warning by gcc
+    (void)res;
+    // recv only sets errno if res is <= 0
+    if (res <= 0){
+      switch (errno) {
+        case EWOULDBLOCK:
+        case ENOENT: //caused by vfs
+          _connected = true;
+          break;
+        case ENOTCONN:
+        case EPIPE:
+        case ECONNRESET:
+        case ECONNREFUSED:
+        case ECONNABORTED:
+          _connected = false;
+          DEBUG_PRINTF("Disconnected: RES: %d, ERR: %d", res, errno);
+          break;
+        default:
+          DEBUG_PRINTF("Unexpected: RES: %d, ERR: %d", res, errno);
+          _connected = true;
+          break;
+      }
+    } else {
+      _connected = true;
+    }
+  }
+  return _connected;
+}
---
+++ libraries/WiFi/src/WiFiClient.h
@@ -0,0 +1,61 @@
+/*
+  Client.h - Base class that provides Client
+  Copyright (c) 2011 Adrian McEwen.  All right reserved.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#pragma once
+
+#include <Arduino.h>
+#include "api/Client.h"
+#include "cbuf.h"
+
+#define TCP_TX_PACKET_MAX_SIZE 1436
+#define TCP_RX_PACKET_MAX_SIZE 1436
+
+class WiFiClient : public Client {
+private:
+  uint32_t _timeoutMs;
+  int _sockfd;
+
+  cbuf *_rxBuff;
+
+  bool _connected;
+public:
+  WiFiClient();
+  ~WiFiClient();
+  int connect(IPAddress ip, uint16_t port);
+  int connect(IPAddress ip, uint16_t port, uint32_t timeoutMs);
+  int connect(const char *host, uint16_t port);
+  size_t write(uint8_t);
+  size_t write(const uint8_t *buf, size_t size);
+  using Print::write;
+
+  int available();
+  int read();
+  int read(uint8_t *buf, size_t size);
+  int peek();
+  void flush();
+  void stop();
+  uint8_t connected();
+
+  operator bool()
+  {
+      return connected();
+  }
+protected:
+  // uint8_t* rawIPAddress(IPAddress& addr) { return addr.raw_address(); };
+};
